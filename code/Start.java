9bb8e8bd70d857d948bc82c39441b1242457e032
Voragun Supawong <voragun2543@gmail.com>
2022-09-19 16:12:50 +0700

    example use @Deprecated

--- code/Start.java
+++ code/Start.java
@@ -1,5 +1,17 @@
 class Start {
     public static void main(String[] data) {
+        Cashier c = new Cashier();
+        c.show();
+    }
+}
+
+class Cashier {
+    @Deprecated
+    String location;
+    int id;
+    
+    @Deprecated
+    void show() {
         
     }
 }
