74d853b3ef43697eac6fa94b9a92c7b8966e36f1
Voragun Supawong <voragun2543@gmail.com>
2022-09-19 16:45:46 +0700

    create bean

--- code/Start.java
+++ code/Start.java
@@ -1,17 +1,35 @@
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 class Start {
     public static void main(String[] data) {
-        Cashier c = new Cashier();
-        c.show();
+        ApplicationContext context;
+        context = new AnnotationConfigApplicationContext(Setup.class);
     }
 }
 
-class Cashier {
-    @Deprecated
-    String location;
-    int id;
+
+@Configuration
+class Setup {     // This is a configuration class
+    @Bean
+    Team createFirst() {
+        Team t = new Team();
+        t.setName("Vortex");
+        t.point = 15;
+        return t;           // Spring will keep this address
+    }
+}
+
+
+class Team {
+    String name;   // field, not a property
+    int point;     // field, not a property
     
-    @Deprecated
-    void show() {
-        
+    // this is property (get, set)
+    public void setName(String s) {
+        name = s;
     }
 }
